4eafe2f358a2129fa46eaf980be6e63a520f8a7e
Jesus <jesuspineda.1996@gmail.com>
2020-02-16 23:21:56 -0600

    adding functionality with async-await

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 import {URL} from "./js/script.js";
-async function MARVElAPI(url){
+const content = document.getElementById('content');
+async function MARVEl_API(url){
    const response  =  await fetch(url);
    const data = response.json();
     if(!response.ok)
-        throw Error("Falla al cargar los datos");
+        throw Error("We have a error!");
     return data;
 }
-(async function () {
+const drawHero = e =>{
+    e.data.results.map( (hero) =>  {
+        const image = `${hero.thumbnail.path}/portrait_uncanny.${hero.thumbnail.extension}`;
+        const superHero= `
+            <div class='content-heroes'>
+                <h2 class='tittle-name'>${hero.name}</h2>
+                <img class='img-hero' src='${image}' alt='${hero.name}'>
+                <p class='description'>${hero.description}</p>
+            </div>
+            `;
+        content.insertAdjacentHTML('beforeEnd',superHero);
+    })
+};
+
+(async  () => {
     try{
-      const data = await MARVElAPI(URL);
-        console.log(data.data.results);
+      const data = await MARVEl_API(URL);
+       drawHero(data);
     }catch (e) {
         console.log(`Error: ${e}`);
     }
